ba6ad03535bc983f016df089b56a0e0d4dde6203
Ivan Ramljak <ivanoramljak@gmail.com>
2026-04-08 12:44:56 +0200

    feat(wt-link): add formula v1.0.0

---
+++ Formula/wt-link.rb
@@ -0,0 +1,18 @@
+class WtLink < Formula
+  desc "WordPress worktree provisioner for local development with Laravel Herd"
+  homepage "https://github.com/piqusy/wt-link"
+  url "https://github.com/piqusy/wt-link/releases/download/v1.0.0/wt-link"
+  sha256 "79d973b035f3939581a2bcc0512f9827464d65ca100d92b267fdb76f1d32f43b"
+  version "1.0.0"
+  license "MIT"
+
+  bottle :unneeded
+
+  def install
+    bin.install "wt-link"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/wt-link --version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,19 @@
+# piqusy/homebrew-tap
+
+Homebrew tap for [piqusy](https://github.com/piqusy) tools.
+
+## Install
+
+```bash
+brew tap piqusy/tap
+```
+
+Then install any tool:
+
+| Formula | Description |
+|---------|-------------|
+| [wt-link](https://github.com/piqusy/wt-link) | WordPress worktree provisioner for local dev with Laravel Herd |
+
+```bash
+brew install piqusy/tap/wt-link
+```
