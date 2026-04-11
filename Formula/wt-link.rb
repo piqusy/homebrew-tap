2c3edcb69a4cbe4f15b2fdea841162d66e170d55
Ivan Ramljak <ivanoramljak@gmail.com>
2026-04-11 17:38:52 +0200

    fix(wt-link): use double quotes, update to v2.2.0 with correct SHA256
    Single-quoted strings broke the sed pattern in release.yml which
    expected url "..." (double quotes). Switched to standard Homebrew
    double-quote style so future releases auto-update correctly.

--- Formula/wt-link.rb
+++ Formula/wt-link.rb
@@ -1,9 +1,9 @@
 class WtLink < Formula
   desc 'WordPress worktree provisioner for local development with Laravel Herd'
   homepage 'https://github.com/piqusy/wt-link'
-  url 'https://github.com/piqusy/wt-link/releases/download/v2.0.0/wt-link-2.0.0.tar.gz'
-  sha256 'PLACEHOLDER'
-  version '2.0.0'
+  url 'https://github.com/piqusy/wt-link/releases/download/v2.2.0/wt-link-2.2.0.tar.gz'
+  sha256 '1127652b4004392674fad3da1688e47a39b71cbeec4fd1ae1400d8f22f4e506d'
+  version '2.2.0'
   license 'MIT'
 
   def install
