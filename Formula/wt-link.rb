b2c5cf6155184fe61d7dfedd887bb5cd9ebe385b
Ivan Ramljak <ivanoramljak@gmail.com>
2026-04-11 18:04:21 +0200

    fix(wt-link): bump to v2.4.0, fix quotes for auto-update sed compatibility

--- Formula/wt-link.rb
+++ Formula/wt-link.rb
@@ -1,14 +1,14 @@
 class WtLink < Formula
-  desc 'WordPress worktree provisioner for local development with Laravel Herd'
-  homepage 'https://github.com/piqusy/wt-link'
-  url 'https://github.com/piqusy/wt-link/releases/download/v2.2.0/wt-link-2.2.0.tar.gz'
-  sha256 '1127652b4004392674fad3da1688e47a39b71cbeec4fd1ae1400d8f22f4e506d'
-  version '2.2.0'
-  license 'MIT'
+  desc "WordPress worktree provisioner for local development with Laravel Herd"
+  homepage "https://github.com/piqusy/wt-link"
+  url "https://github.com/piqusy/wt-link/releases/download/v2.4.0/wt-link-2.4.0.tar.gz"
+  sha256 "131c170078974458ff5495e596c4a277398e311fc1108c0eefba8fc5294d964b"
+  version "2.4.0"
+  license "MIT"
 
   def install
-    bin.install 'bin/wt-link'
-    (lib / 'wt-link').install Dir['lib/wt-link/*.sh']
+    bin.install "bin/wt-link"
+    (lib / "wt-link").install Dir["lib/wt-link/*.sh"]
   end
 
   test do
