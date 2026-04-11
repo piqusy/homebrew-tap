33fd3f868c49bc5509fccc64d994ed09440e5474
Ivan Ramljak <ivanoramljak@gmail.com>
2026-04-11 10:22:55 +0200

    update(wt-link): migrate to tarball install for v2.0.0
    - URL now points to wt-link-VERSION.tar.gz (bin/ + lib/wt-link/ tarball)
    - Install bin/wt-link into Homebrew bin prefix
    - Install lib/wt-link/*.sh into Homebrew lib/wt-link/
    - SHA256 placeholder will be updated by release workflow on tag push

--- Formula/wt-link.rb
+++ Formula/wt-link.rb
@@ -1,13 +1,14 @@
 class WtLink < Formula
-  desc "WordPress worktree provisioner for local development with Laravel Herd"
-  homepage "https://github.com/piqusy/wt-link"
-  url "https://github.com/piqusy/wt-link/releases/download/v1.7.1/wt-link"
-  sha256 "fe2d20db01f0d6c302db7318eea8aa0a9508f80fe704e1c2bbfc14b4c181ba97"
-  version "1.7.1"
-  license "MIT"
+  desc 'WordPress worktree provisioner for local development with Laravel Herd'
+  homepage 'https://github.com/piqusy/wt-link'
+  url 'https://github.com/piqusy/wt-link/releases/download/v2.0.0/wt-link-2.0.0.tar.gz'
+  sha256 'PLACEHOLDER'
+  version '2.0.0'
+  license 'MIT'
 
   def install
-    bin.install "wt-link"
+    bin.install 'bin/wt-link'
+    (lib / 'wt-link').install Dir['lib/wt-link/*.sh']
   end
 
   test do
